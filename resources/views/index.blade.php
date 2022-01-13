39c0e7483699cc7ddd1da49cf8eafe3e3cb055f0
harry-so <tst730_intelath_hiro@outlook.jp>
2022-01-13 22:11:12 +0900

    back

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,6 +4,8 @@
 
   @include('partials.home.hero')
   <div class="clearfix"></div>
-  
+  @include('partials.home.feature')
+  @include('partials.home.seller')
+  @include('partials.home.collection')
 
 @endsection
