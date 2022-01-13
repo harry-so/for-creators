d205c28726603852a383229657b785f807b1a168
harry-so <tst730_intelath_hiro@outlook.jp>
2022-01-13 22:10:04 +0900

    try

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,8 +4,6 @@
 
   @include('partials.home.hero')
   <div class="clearfix"></div>
-  @include('partials.home.feature')
-  @include('partials.home.seller')
-  @include('partials.home.collection')
+  
 
 @endsection
